44fd238d13d4a2f538bd2b45a6d2bf6130f947fd
El Mehdi Tabet <el-mehdi.tabet.1@ens.etsmtl.ca>
2026-02-03 10:32:07 -0500

    adding KafkaConsumer logic and new Analytics-services with logs that confirms everything work.. also dockerisation of the new service.

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.pm.patientservice.kafka;
 
 import com.pm.patientservice.model.Patient;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
@@ -11,7 +12,7 @@ import patient.events.PatientEvent;
 public class KafkaProducer {
 
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
-    private Logger log;
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate){
         this.kafkaTemplate = kafkaTemplate;
